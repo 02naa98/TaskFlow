ef101591148a6a0114fd954852f97a2fdd10acf2
Githubに登録したユーザー名 <Github>
2024-10-16 21:35:13 +0900

    基本機能の実装

--- todoproject/todo_app/views.py
+++ todoproject/todo_app/views.py
@@ -1,3 +1,4 @@
+from django.db.models.query import QuerySet
 from django.urls import reverse_lazy
 from django.views.generic import CreateView,ListView
 from .models import TaskCreate
@@ -6,14 +7,17 @@ from .forms import TaskForm
 class TaskCreateView(CreateView):
     model = TaskCreate #表示するモデル
     form_class = TaskForm #表示するフォーム
-    template_name = 'task_form.html'  # フォームを表示するテンプレートの指定
+    template_name = 'todo_app/task_create.html'  # フォームを表示するテンプレートの指定
     success_url = reverse_lazy('task_list')  # 作成成功後のリダイレクト先
 
     def form_valid(self, form):
+        form.instance.user=self.request.user #現在のユーザーを設定
         # フォームが有効な場合の処理
         return super().form_valid(form)
+    def get_queryset(self): #現在ログインしているユーザーが作成したタスクを取得
+        return TaskCreate.objects.filter(user=self.request.user)
 
 class TaskListView(ListView):
     model = TaskCreate  # 表示するモデル
-    template_name = 'task_list.html'  # 使用するテンプレート
+    template_name = 'todo_app/task_list.html'  # 使用するテンプレート
     context_object_name = 'tasks'  # テンプレートで使用するオブジェクト名
