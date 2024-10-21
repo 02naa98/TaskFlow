eda0c3b47a90e6d401ea8882617d67387bce2e9a
Githubに登録したユーザー名 <Github>
2024-10-21 21:15:42 +0900

    sort starの途中

--- todoproject/todo_app/urls.py
+++ todoproject/todo_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import TaskCreateView,TaskListView,ListCreateView
+from .views import TaskCreateView,TaskListView,ListCreateView,ToggleStarView
 
 urlpatterns = [
     path('tasks/', TaskListView.as_view(), name='task_list'),  # タスク一覧
-    path('tasks/test/', TaskListView.as_view(), name='task_test'), #tasks by listで使ってた
-    path('tasks/<int:list_id>/', TaskListView.as_view(), name='task_list_by_list'),
     path('list/create/', ListCreateView.as_view(), name='list_create'),  #リスト作成
     path('task/create/', TaskCreateView.as_view(), name='task_create'),  #タスク作成
+    #動的url(ページ遷移が小さい)
+    path('tasks/<int:list_id>/', TaskListView.as_view(), name='task_list_by_list'),
+    path('tasks/toggle_star/<int:task_id>/', ToggleStarView.as_view(), name='toggle_star'),
 ]
