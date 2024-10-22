01b885102d7ec96de419872bc94d5ffbea51596c
Githubに登録したユーザー名 <Github>
2024-10-22 21:29:39 +0900

    タスクのdeleteまで、次は質問

--- todoproject/todo_app/urls.py
+++ todoproject/todo_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import TaskCreateView,TaskListView,ListCreateView,ToggleStarView
+from .views import TaskCreateView,TaskListView,TaskUpdateView,TaskDeleteView,ListCreateView,ToggleStarView
 
 urlpatterns = [
     path('tasks/', TaskListView.as_view(), name='task_list'),  # タスク一覧
     path('list/create/', ListCreateView.as_view(), name='list_create'),  #リスト作成
     path('task/create/', TaskCreateView.as_view(), name='task_create'),  #タスク作成
+    path('tasks/<int:pk>/update/', TaskUpdateView.as_view(), name='task_update'), #subbranch2
+    path('tasks/<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'), #subbranch3
     #動的url(ページ遷移が小さい)
     path('tasks/<int:list_id>/', TaskListView.as_view(), name='task_list_by_list'),
     path('tasks/toggle_star/<int:task_id>/', ToggleStarView.as_view(), name='toggle_star'),
